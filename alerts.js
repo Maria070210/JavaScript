c3aef8e656f311f9277591d6e0e947ada6364f95
Maria070210 <mcatala2002@gmail.com>
2025-11-21 12:01:13 +0100

    He añadido alertas al archivo de alerts.js

---
+++ alerts.js
@@ -0,0 +1,7 @@
+alert('Hola Mundo'); // Mensaje de alerta
+
+prompt('¿Cuál es tu nombre?'); // Es como una alerta pero donde puedes escribir en un cuadro de texto y darle a Aceptar o Cancelar
+prompt('¿Cuál es tu nombre?', 'Sin nombre'); // Lo de sin nombre es lo que va a estar escrito en el cuadro de texto por defecto
+let nombre = prompt('¿Cuál es tu nombre?', 'Sin nombre'); //Se almacena lo que se escriba en el cuadro de texto
+
+confirm('¿Está seguro de borrar esto?'); // Alerta de confirmación, si das a Aceptar es true si le das a Cancelar false
--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let a = 10; //Preferible
-var b = 10; //Forma antigua
-const c = 10; //No se puede modificar su valor en un futuro
+let a = 10; // Preferible
+var b = 10; // Forma antigua
+const c = 10; // No se puede modificar su valor en un futuro
 
 let d = 10, 
     e = 20,
     f = 30,
     x = d + f;
 
-//console.log('%c Mis variables', 'color:blue; font-weight: bold'); //Para ponerle estilos de css
-console.log({a}); //Mostrar el log de la consola
-console.warn({e}); //Para mostrar un warning/advertencia
-console.error({f}); //Para mostrar un error, parece un error
-//{e} --> para que salga variable y valor
-console.table({a, e, d, f}); //Crea una tabla separada por variable y valor
+// console.log('%c Mis variables', 'color:blue; font-weight: bold'); //Para ponerle estilos de css
+console.log({a}); // Mostrar el log de la consola
+console.warn({e}); // Para mostrar un warning/advertencia
+console.error({f}); // Para mostrar un error, parece un error
+//{e} --> Para que salga variable y valor
+console.table({a, e, d, f}); // Crea una tabla separada por variable y valor
 
-window.outerHeight; //Te da la altura
-window.outerWidth; //Te da el ancho
+window.outerHeight; // Te da la altura
+window.outerWidth; // Te da el ancho
