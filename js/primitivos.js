ae4a0240fa6deb47d173367768d4be71dceea885
Maria070210 <mcatala2002@gmail.com>
2025-11-21 13:26:27 +0100

    He creado el archivo protipRetorno.js

---
+++ js/primitivos.js
@@ -0,0 +1,8 @@
+let nombre = 'Park Jimin';
+nombre = 'Jimin';
+
+console.log(typeof nombre); // Devuelve el tipo de variable, en este caso String
+
+let boleano = true;
+
+let soyNull = null; // Objeto
